ccb6f2121edc24b5cb38b10d909a9c0c24b97e60
QuintenS-PC <quinten.schutyser@gmail.com>
2019-05-30 01:24:31 +0200

    toevoegen van de api. Zeer simple

---
+++ Project_API2/Controllers/FavoriteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project_API2.Controllers
+{
+    [Route("api/2")]
+    public class FavoriteController : Controller
+    {
+        [Route("test")]
+        public IActionResult Index()
+        {
+            return Content("Hello");
+        }
+    }
+}
