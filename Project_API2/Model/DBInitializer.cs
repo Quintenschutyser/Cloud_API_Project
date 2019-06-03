17729fea272b4980020aa5d07eb64f1a18a302af
QuintenS-PC <quinten.schutyser@gmail.com>
2019-06-03 17:39:30 +0200

    controller en model voor User werken

--- Project_API2/Model/DBInitializer.cs
+++ Project_API2/Model/DBInitializer.cs
@@ -25,8 +25,45 @@ namespace Project_API2.Model
                     Currency = "EUR",
                     Alpha3Code = "NLD"
                 };
+                var l3 = new Land()
+                {
+                    Name = "France",
+                    Currency = "EUR",
+                    Alpha3Code = "FRA"
+                };
+                var l4 = new Land()
+                {
+                    Name = "Australia",
+                    Currency = "AUD",
+                    Alpha3Code = "AUS"
+                };
+                //var u3 = new Users()
+                //{
+                //    firstName = "testFirstName2",
+                //    lastName = "testLastName2",
+                //    liked = { l4 }
+                //};
                 context.Land.Add(l1);
                 context.Land.Add(l2);
+                context.Land.Add(l3);
+                context.Land.Add(l4);
+                //context.Users.Add(u3);
+                context.SaveChanges();
+            }
+            if (!context.Users.Any())
+            {
+                var u1 = new Users()
+                {
+                    firstName = "Quinten",
+                    lastName = "Schutyser"
+                };
+                var u2 = new Users()
+                {
+                    firstName = "testFirstName",
+                    lastName = "testLastName"
+                };
+                context.Users.Add(u1);
+                context.Users.Add(u2);
                 context.SaveChanges();
             }
         }
